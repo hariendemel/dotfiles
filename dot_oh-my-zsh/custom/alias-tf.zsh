1ed511113b8f235f129c30d4bcb3be4ce2039d50
Harien de Mel <mail@harien.dev>
2025-01-16 09:41:32 +1300

    oh-my-zsh: move tf alias to own file

---
+++ dot_oh-my-zsh/custom/alias-tf.zsh
@@ -0,0 +1,17 @@
+#Terraform aliases for ZSH
+
+#Terraform
+alias t="terraform"
+alias ti="t init"
+alias tp="t plan"
+alias ta="t apply"
+alias tc="t console"
+alias tdes="t destroy"
+alias tim="t import"
+alias ts="t state"
+alias tsl="ts list"
+alias tss="ts show"
+alias tsrm="ts rm"
+alias tsmv="ts mv"
+alias tw="t workspace"
+
--- dot_oh-my-zsh/custom/alias.zsh
+++ dot_oh-my-zsh/custom/alias.zsh
@@ -16,18 +16,3 @@ alias gcr="gco --track"
 alias gcob="gco -b"
 alias grguo="gr get-url origin"
 
-#Terraform
-alias t="terraform"
-alias ti="t init"
-alias tp="t plan"
-alias ta="t apply"
-alias tc="t console"
-alias tdes="t destroy"
-alias tim="t import"
-alias ts="t state"
-alias tsl="ts list"
-alias tss="ts show"
-alias tsrm="ts rm"
-alias tsmv="ts mv"
-alias tw="t workspace"
-
