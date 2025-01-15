e9ee76a71f653b953e193912bcc19bd92512fee9
Harien de Mel <mail@harien.dev>
2025-01-16 10:54:25 +1300

    oh-my-zsh: alias for foobar codeartifact login

--- dot_oh-my-zsh/custom/alias-fb.zsh
+++ dot_oh-my-zsh/custom/alias-fb.zsh
@@ -1,4 +1,10 @@
 #forsyth barr specific aliases
 
-alias aca-auth="$HOME/src/app-deploy/trove-scripts/codeartifact/codeartifact_auth.py"
+function setup_codeartifact_creds() {
+    aws codeartifact login --tool=pip --domain tooling --repository serving
+    export POETRY_HTTP_BASIC_TROVE_USERNAME=aws
+    export POETRY_HTTP_BASIC_TROVE_PASSWORD=$(aws codeartifact get-authorization-token --domain-owner 335433835758 --domain tooling --query 'authorizationToken' --output text)
+}
+
+alias py-auth=setup_codeartifact_creds
 
