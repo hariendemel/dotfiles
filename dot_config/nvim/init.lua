d93499fef4961c9a90e27d3c063b71225fa042dc
Harien de Mel <mail@harien.dev>
2026-07-25 10:23:26 +1200

    neovim: rename settings > options and set options

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,3 @@
-require("config.setting")
+require("config.options")
 require("config.lazy")
 
---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
