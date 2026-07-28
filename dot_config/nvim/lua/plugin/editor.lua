03f3c8dc6a68b15d05ec954e578c8ed032b3e44e
Harien de Mel <mail@harien.dev>
2026-07-28 16:39:22 +1200

    neovim: enable markdown rendering

--- dot_config/nvim/lua/plugin/editor.lua
+++ dot_config/nvim/lua/plugin/editor.lua
@@ -1,5 +1,6 @@
 vim.pack.add {
   "https://github.com/nvim-mini/mini.nvim",
+  "https://github.com/MeanderingProgrammer/render-markdown.nvim",
 }
 
 require("mini.comment").setup()
@@ -12,5 +13,9 @@ require("mini.completion").setup({
   lsp_completion = {
     auto_setup = true,
   }
+})
+
+require('render-markdown').setup({
+    completions = { lsp = { enabled = true } },
 })
 
