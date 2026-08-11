d5bb3af9574559b2316dc0aa2046bd5b4f84ee80
Harien de Mel <mail@harien.dev>
2026-08-12 09:49:58 +1200

    nvim: ignore branch name in session management

--- dot_config/nvim/lua/plugin/session.lua
+++ dot_config/nvim/lua/plugin/session.lua
@@ -9,8 +9,8 @@ local autoSession = require("auto-session")
 autoSession.setup({
   auto_restore_last_session = true,
   show_auto_restore_notif = true,
-  git_use_branch_name = true,
-  git_auto_restore_on_branch_change = true,
+  git_use_branch_name = false,
+  git_auto_restore_on_branch_change = false,
 })
 
 vim.keymap.set("n", "<leader>sf", "<cmd>AutoSession search<CR>", { desc = "Session search" })
